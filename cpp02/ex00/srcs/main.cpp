d15ee428f89ced1c1e4559d1b82680871193cf8b
Luna DINAUT <ldinaut@e2r10p13.clusters.42paris.fr>
2022-10-10 15:48:30 +0200

    modified gitignore + update Makefile

---
+++ cpp02/ex00/srcs/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 19:26:14 by ldinaut           #+#    #+#             */
+/*   Updated: 2022/10/10 15:17:50 by ldinaut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main( void ) 
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+return 0;
+}
