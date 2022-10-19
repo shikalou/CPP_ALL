bfe795431034638ef83d4cfd088f504b6de0e2d8
Luna DINAUT <ldinaut@e2r13p10.clusters.42paris.fr>
2022-10-19 19:00:37 +0200

    nique

--- cpp03/ex00/includes/ClapTrap.hpp
+++ cpp03/ex00/includes/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 16:51:59 by ldinaut           #+#    #+#             */
-/*   Updated: 2022/10/14 17:41:35 by ldinaut          ###   ########.fr       */
+/*   Updated: 2022/10/19 18:57:46 by ldinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,11 @@ class ClapTrap
 	void	attack(const std::string &target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
+	int	getHP(void);
+	int	getMana(void);
+	int	getDamage(void);
+	std::string getName(void);
+	ClapTrap	&operator=(ClapTrap const &egal);
 
 	private:
 	std::string _name;
--- cpp03/ex00/srcs/ClapTrap.cpp
+++ cpp03/ex00/srcs/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 16:51:24 by ldinaut           #+#    #+#             */
-/*   Updated: 2022/10/14 18:19:08 by ldinaut          ###   ########.fr       */
+/*   Updated: 2022/10/19 19:00:17 by ldinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,32 @@ ClapTrap::ClapTrap(ClapTrap const &copy)
 ClapTrap::~ClapTrap()
 {
 	std::cout << "Destructor called" << std::endl;
+}
+
+int	ClapTrap::getHP(void)
+{
+	return (_hp);
+}
+
+int	ClapTrap::getMana(void)
+{
+	return (_mana);
+}
+
+int	ClapTrap::getDamage(void)
+{
+	return (_damage);
+}
+
+std::string ClapTrap::getName(void)
+{
+	return (_name);
+}
+
+ClapTrap	&ClapTrap::operator=(ClapTrap const &egal)
+{
+	_name = egal.getName();
+
 }
 
 void	ClapTrap::attack(const std::string &target)
